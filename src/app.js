063dafcb528b55220ab27814beddb9ac15ba68fd
Sam Bradshaw <samothreesixty@gmail.com>
2026-02-23 20:46:55 +0000

    Initialise the authStore in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import '@src/bootstrap';
-import '@src/styles/app.css';
+import '@/bootstrap';
+import '@/styles/app.css';
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
+import {useAuthStore} from '@/stores/auth';
 
 import router from './routes/index.js';
 import App from './app.vue';
@@ -9,6 +10,10 @@ import App from './app.vue';
 const app = createApp(App);
 const pinia = createPinia();
 
-app.use(router);
 app.use(pinia);
+app.use(router);
+
+const authStore = useAuthStore(pinia);
+await authStore.verify();
+
 app.mount('#app');
