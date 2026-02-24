9c46155080dad65d79001bbd8a8eb26b02c07f33
Sam Bradshaw <samothreesixty@gmail.com>
2026-02-24 00:08:26 +0000

    Corrected password.reset route to a web route rather than api (duh)

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware(['signed', 'throttle:verification-link'])
     ->name('verification.verify');
 
+Route::get('/reset-password/{token}', function () {
+    return view('welcome');
+})->name('password.reset');
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
